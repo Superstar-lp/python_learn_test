97b82c3c1e2ed027d85aba02b79cb888b452e4e3
liupeng <liupeng.succeed@foxmail.com>
2023-02-13 16:49:23 +0800

    new file:   Loop and branch.py

---
+++ Loop and branch.py
@@ -0,0 +1,23 @@
+###Loop and branch 1
+# if 3 > 5:
+#     print ("我在里面")
+#     print("我在外面")
+# print("我真在外面")
+
+# 多条件分支
+# score = input("请输入分数：")
+# score = int(score)
+
+# if 0 <= score< 60:
+#     print("D")
+# elif 60<= score <80:
+#     print("C")
+# elif 80<= score <90:
+#     print("B")
+# elif 90<= score <100:
+#     print("A")
+# elif score==100:
+#     print("S")
+# else:
+#     print("请输入0~100的分值！")
+###Loop and branch 2
