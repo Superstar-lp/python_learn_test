5eb2a160bf47f2ed16f193f65b25ee5aab17fbd9
liupeng <liupeng.succeed@foxmail.com>
2023-03-10 23:10:10 +0800

    new file:   "image/python\347\254\224\350\256\260(\344\273\216\346\240\274\345\274\217\345\214\226\345\255\227\347\254\246\344\270\262\345\274\200\345\247\213)/1678459801421.png" 	renamed:    string_1.py -> p28 string_1.py 	renamed:    string_2.py -> p29 string_2.py 	renamed:    string_3.py -> p30 string_3.py 	renamed:    string_4.py -> p31 string_4.py 	renamed:    string_5(format).py -> p32 string_5(format).py 	renamed:    string_6.py -> p33 string_6.py 	modified:   p39_dictionary_3.py 	modified:   "python\347\254\224\350\256\260(\344\273\216\346\240\274\345\274\217\345\214\226\345\255\227\347\254\246\344\270\262\345\274\200\345\247\213).md"

--- p39_dictionary_3.py
+++ p39_dictionary_3.py
@@ -1,14 +1,53 @@
-d = dict.fromkeys("FishC")
-print(d)
-d['s']=115
-print(d)
+# d = dict.fromkeys("FishC")
+# print(d)
+# d['s']=115
+# print(d)
 
-# update([other])
-d.update({'i':105,'h':104})
-print(d)
-d.update(F='70',C='67')
-print(d)
+# # update([other])
+# d.update({'i':105,'h':104})
+# print(d)
+# d.update(F='70',C='67')
+# print(d)
 
-## 查
-d['s']
-print(d.get('c',"这里没有c"))
+# ## 查
+# d['s']
+# print(d.get('c',"这里没有c"))
+
+# print(d.setdefault('C',"code"))
+# print(d.setdefault('c',"code"))
+# print(d)
+
+# ##  items()、keys()  values()
+# keys = d.keys()
+# values = d.values()
+# items = d.items()
+# print(items)
+# print(keys)
+# print(values)
+# d.pop('c')
+# print(items)
+# print(keys)
+# print(values)
+
+# # copy()
+# e = d.copy()
+# print(e)
+
+# # len() 获取字典中键值对的数量
+# len(d)
+# # 用 in 和 not in 判断键是否在字典中
+# 'C' in d
+# "c" not in d
+# print(list(d))
+
+# e = iter(d)   # 迭代器
+# print(next(e))
+# print(next(e))
+# print(next(e))
+
+# print(list(reversed(d.values())))
+
+# d = {"吕布":{"语文":60,"数学":70,"英语":80}, "关羽": {"语文":80, "数学":90,"英语":70}}
+# print(d["吕布"]["数学"])
+d = {"吕布":{60,70,80}, "关羽": {80, 90,70}}
+print(d["吕布"][1])
